dd39afae13caf5ba79f0aff07f28f430edb100e7
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-16 16:08:39 +0800

    remove floating action button to navigate to calendar

--- app/src/main/java/com/ralphmarondev/velora/features/dashboard/presentation/DashboardScreen.kt
+++ app/src/main/java/com/ralphmarondev/velora/features/dashboard/presentation/DashboardScreen.kt
@@ -14,11 +14,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.rounded.Construction
 import androidx.compose.material.icons.rounded.Traffic
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme

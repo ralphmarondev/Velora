434e04d7ca0ddc4426984486bd7688c1634d455e
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-17 10:42:00 +0800

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.ralphmarondev.velora"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         manifestPlaceholders["MAPS_API_KEY"] = localProperties["MAPS_API_KEY"]?.toString() ?: ""
